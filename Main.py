dcd0cfd9bad234e06f063ae966b869e5424d0cf2
Danilo A. Santos <danilo_as1992@hotmail.com>
2022-04-24 16:10:48 -0300

    update

--- Main.py
+++ Main.py
@@ -1,33 +1,50 @@
 import os
-
+import sys
 # Enable GPU:
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-
-# Disable GPU
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import datetime
 import time
 import glob
 import cv2
 import numpy as np
+import pandas as pd
 from enum import Enum
 from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras.callbacks import TensorBoard
-from tensorflow_core.python.keras.utils import np_utils
+from sklearn.model_selection import KFold, GroupShuffleSplit
+from sklearn.neural_network import MLPClassifier
+from sklearn import metrics
+from keras.utils import np_utils
 from tensorflow.python.client import device_lib
+from skmultilearn.model_selection import iterative_train_test_split
 
-#Ignore:
-#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-#sess = tf.Session(config=tf.ConfiProto(gpu_options=gpu_options))
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.6
+gpus = tf.config.experimental.list_physical_devices('GPU')
 
-#Disable GPU:
-#sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(device_count={'GPU': 0}))
+if gpus:
+
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
+device_name = tf.test.gpu_device_name()
+
+if device_name != '/device:GPU:0':
+    print('ATENTION: GPU device not found. CPU will be used!')
+else:
+    print('Found GPU at: {}'.format(device_name))
+
 
 class Menu:
     menu_option = {'1': 'Generate dataset',
@@ -113,6 +130,7 @@ def get_label(emotion):
 
 
 def create_dataset(dataset, dir_files, dimension_resized):
+
     for dir in glob.iglob(dir_files + '**/*', recursive=False):
 
         try:
@@ -126,8 +144,8 @@ def create_dataset(dataset, dir_files, dimension_resized):
             image_resized = cv2.resize(image, dimension_resized)
             dataset.append(Face(image_resized, emotion_face, label))
 
-        except Exception as e:
-            pass
+        except Exception as err:
+            print('Error creating dataset: ' + err)
 
 
 def delete_dataset():
@@ -147,7 +165,7 @@ def get_dataset_data(dataset, dimension):
         x.append(np.reshape(image_array, dimension))
         y.append(face.get_label())
 
-    y = np.reshape(y, (len(y),1))
+    y = np.reshape(y, (len(y), 1))
     return x, y
 
 
@@ -164,9 +182,9 @@ def train_data(features, labels):
         for size in sizes_layers:
             for conv in conv_layers:
 
-                name_model = 'Training_Model_{}_Dense_{}_Size_{}_Conv_{}'.format(dense,size,conv,int(time.time()))
+                name_model = 'Training_Model_{}_Dense_{}_Size_{}_Conv_{}'.format(dense, size, conv, int(time.time()))
 
-                tensorboard = TensorBoard(log_dir='logs\\{}'.format(name_model))
+                tensorboard = TensorBoard(log_dir='logs/{}'.format(name_model))
 
                 model = Sequential()
 
@@ -194,34 +212,111 @@ def train_data(features, labels):
                 print(model.summary())
                 model.fit(features, y_labels, batch_size=32, epochs=25, validation_split=0.1, callbacks=[tensorboard])
 
-    '''model = Sequential()
 
-    model.add(Conv2D(64, (3, 3), input_shape=features.shape[1:]))
-    model.add(Activation("relu"))
-    model.add(MaxPool2D(pool_size=(2, 2)))
+def train_data_dev(features, labels):
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation("relu"))
-    model.add(MaxPool2D(pool_size=(2, 2)))
+    features = features / 255.0
+    y_labels = np_utils.to_categorical(labels)
 
-    model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation('relu'))
+    kf = KFold(3, shuffle=True, random_state=42)
+    gss = GroupShuffleSplit(n_splits=3, random_state=42)
 
-    model.add(Dense(7))
-    model.add(Activation('softmax'))
+    fold = 0
+    y_test_values = []
+    result_predict = []
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    for train, test in kf.split(features, y_labels):
 
-    print(model.summary())
+        fold += 1
 
-    model.fit(features, y_labels, batch_size=32, epochs=40, validation_split=0.1, callbacks=[tensorboard])'''
+        x_train = features[train]
+        y_train = y_labels[train]
+        x_test = features[test]
+        y_test = y_labels[test]
+
+        print('\n### TRAINNING FOLD {} \nX: {} \nY: {} '.format(str(fold), str(x_train.shape), str(y_train.shape)))
+
+        model = Sequential()
+
+        model.add(Conv2D(64, (3, 3), input_shape=features.shape[1:]))
+        model.add(Activation("relu"))
+        model.add(MaxPool2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation("relu"))
+        model.add(MaxPool2D(pool_size=(2, 2)))
+
+        model.add(Flatten())
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+
+        model.add(Dense(7))
+        model.add(Activation('softmax'))
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        print(model.summary())
+
+        tensorboard = TensorBoard(log_dir='logsDev/{}'.format('Training_Model_Dev'))
+        model.fit(x_train, y_train, batch_size=512, epochs=3, validation_split=0.1, callbacks=[tensorboard])
+
+        pred = model.predict(x_test)
+
+        y_test_values.append(y_test)
+        result_predict.append(pred)
+
+        confusion_matrix = metrics.confusion_matrix(y_test.argmax(axis=1), pred.argmax(axis=1))
+
+        plt.figure(figsize=(20, 20))
+        plt.title('TRAINNING FOLD {} - X: {} - Y: {} '.format(str(fold), str(x_train.shape), str(y_train.shape)))
+        columns = [f'Predicted {label}' for label in ('Afraid', 'Angry', 'Disgusted', 'Happy', 'Neutral', 'Sad', 'Surprised')]
+        indices = [f'Actual {label}' for label in ('Afraid', 'Angry', 'Disgusted', 'Happy', 'Neutral', 'Sad', 'Surprised')]
+
+        table = pd.DataFrame(confusion_matrix, columns=columns, index=indices)
+        sns.heatmap(table, annot=True, fmt='d')
+
+        #sns.heatmap(confusion_matrix, annot=True)
+        plt.yticks(rotation=90)
+        plt.show()
+        sys.exit(0)
+        print(y_test.shape)
+        print(pred.shape)
+
+        score = np.sqrt(metrics.accuracy_score(y_test, pred))
+        print('ACCURACY SCORE: {}'.format(score))
+
+
+def mlp_classifier(features, labels):
+
+    features = features / 255.0
+    y_labels = np_utils.to_categorical(labels)
+    mlp = MLPClassifier(hidden_layer_sizes=(7,), max_iter=25, tol=0.00001, solver='adam', activation='relu')
+
+    kf = KFold(3, shuffle=True, random_state=42)
+
+    fold = 0
+    for train, test in kf.split(features, y_labels):
+        fold += 1
+
+        x_train = features[train]
+        y_train = y_labels[train]
+        x_test = features[test]
+        y_test = y_labels[test]
+
+        print('\n### TRAINNING FOLD {} \nX: {} \nY: {} '.format(str(fold), str(x_train.shape), str(y_train.shape)))
+
+        mlp.fit(x_train, y_train)
+
+        pred = mlp.predict(x_test)
+
+        confusion_matrix = metrics.confusion_matrix(y_test.argmax(axis=1), pred.argmax(axis=1))
+        print(confusion_matrix)
 
 
 def main():
 
     dataset = []
-    dir_image_files = "C:/Users/Danilo/Desktop/DATASETS/KDEF/"
+    dir_image_files = "dataset/KDEF/"
     width = 50
     height = 67
     features = None
@@ -229,6 +324,7 @@ def main():
 
     menu = Menu()
     option = True
+    dev = True
 
     while option:
 
@@ -251,6 +347,7 @@ def main():
                 print('\nLoading dataset...')
                 features = np.load('features.npy')
                 labels = np.load('labels.npy')
+                print('\nDataset loaded...\nX: {} \nY: {} '.format(str(features.shape), str(labels.shape)))
             else:
                 print('Dataset not found! Create a dataset before load it!')
 
@@ -258,8 +355,14 @@ def main():
             if features is None or labels is None:
                 print('You need load a dataset before use it!')
             else:
-                print('\nTraining dataset...')
-                train_data(features, labels)
+
+                if dev:
+                    print('\nMode DEV: Training dataset...')
+                    #mlp_classifier(features, labels)
+                    train_data_dev(features, labels)
+                else:
+                    print('\nTraining dataset...')
+                    train_data(features, labels)
 
         elif option == '4':
             break
